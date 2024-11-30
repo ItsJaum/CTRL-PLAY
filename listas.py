ec579cf2ea2912ecf0b6a03f15c57c926517739f
Savio-xavier-1 <gomesbitellaj@gmail.com>
2024-11-30 15:05:03 -0400

    Listas

---
+++ listas.py
@@ -0,0 +1,34 @@
+# CF = ["JoJo", "scott pilgrim", "Jogos", "Mn", "RPG"]
+# print(CF[3])
+# print(CF[1])
+
+# CF[1] = "O Grilo"
+# print(CF[1])
+
+# print(CF)
+ 
+# CF.append("series")
+# CF.append("celeste")
+# print(CF)
+# CF.insert(3, "sair")
+# print(CF)
+# menosGosto = "series"
+# CF.remove(menosGosto)
+# print(CF)
+# CF.pop()
+# print(CF)
+# CF.sort()
+# print(CF)
+# CF.sort(reversed)
+# print(CF)
+
+# Atividade
+
+Nomes = ["Alisson", "Joao", "Nyny", "Luanna", "Max"]
+print(Nomes[1])
+Nomes[2] = "Paulo"
+Nomes.insert(4, "Fernanda")
+RuimNoBrawlStars = "Alisson"
+Nomes.remove(RuimNoBrawlStars)
+Nomes.pop()
+print(Nomes)
