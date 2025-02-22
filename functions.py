c615cba980dd3525a588d740cd3aa075bfaf9d25
ItsJaum <gomesbitellaj@gmail.com>
2025-02-22 15:54:05 -0300

    dia 22/02

---
+++ functions.py
@@ -0,0 +1,29 @@
+# # def soma(a,b,c):
+# #     return a + b + c
+# # print(soma(5,3,4))
+# # print(soma(5,2,2))
+
+# # def multiplicacao(a,b,c):
+# #     return a * b * c
+# # print(multiplicacao(8,4,2))
+
+# def bemVindo():
+#     print("bien vinido")
+
+# def menor(a,b):
+# if a <=b:
+#     return a
+# else:
+#     return b
+
+# print(menor(25,15))
+
+
+def velocidadeMedia(tempo,distancia):
+    return distancia/tempo
+
+
+distancia = float(input("digite a distancia: "))
+tempo = float(input("digite o tempo: "))
+
+print(velocidadeMedia(tempo,distancia))
