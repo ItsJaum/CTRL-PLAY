7e74a6e4f7330231c5f4a34c73d8bb27c4a0cbff
savioxavi67 <arthur.barella0312@gmail.com>
2025-04-26 15:01:45 -0400

    26/04
    qualquer coisa

---
+++ pilaresdoPOO.py
@@ -0,0 +1,9 @@
+VI = input()
+tipo = input()
+classe = input()
+
+if VI == "vertebrado" and tipo == "ave" and classe == "carnivoro":
+    print("aguia")
+elif VI == "vertebrado" and tipo == "ave" and classe == "onivoro":
+    print("pomba")
+elif VI == "vertebrado" and tipo == "mamifero" and classe = "onivoro":
